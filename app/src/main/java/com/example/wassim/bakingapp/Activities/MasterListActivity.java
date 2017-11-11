42a54c7f603cc34c6d94aac851354c356b99be26
WassimErriha <wassim.erriha@gmail.com>
2017-11-10 23:23:01 -0600

    implement two-pane layout 2

--- app/src/main/java/com/example/wassim/bakingapp/Activities/MasterListActivity.java
+++ app/src/main/java/com/example/wassim/bakingapp/Activities/MasterListActivity.java
@@ -2,12 +2,10 @@ package com.example.wassim.bakingapp.Activities;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
-
 import com.example.wassim.bakingapp.Objects.Recipe;
 import com.example.wassim.bakingapp.R;
 import com.example.wassim.bakingapp.UI.IngredientsListFragment;
@@ -28,25 +26,31 @@ public class MasterListActivity extends AppCompatActivity implements MasterListF
 
         if (findViewById(R.id.two_pane_activity_master_list) != null) {
             twoPaneLayout = true;
-            Toast.makeText(this, " we are in tablet mode", Toast.LENGTH_LONG).show();
+            // show Ingredients fragment
             fragmentManager = getSupportFragmentManager();
-
             fragmentTransaction = fragmentManager.beginTransaction();
             ingredientListFragment = new IngredientsListFragment();
             fragmentTransaction.add(R.id.item_details_container, ingredientListFragment);
             fragmentTransaction.commit();
-
         } else {
             twoPaneLayout = false;
-            Toast.makeText(this, " we are in phone mode", Toast.LENGTH_LONG).show();
         }
-
     }
 
     @Override
     public void onRecyclerViewInteraction(String videoUrl, String stepDescription, String thumbnailUrl) {
         if (twoPaneLayout) {
-            addExoplayerFragment(videoUrl, stepDescription, thumbnailUrl);
+            // show mediaPlayer and instructions fragment
+            fragmentTransaction = fragmentManager.beginTransaction();
+            MediaPlayerWithInstructionsFragment mediaPlayerWithInstructionsFragment = new MediaPlayerWithInstructionsFragment();
+            fragmentTransaction.replace(R.id.item_details_container, mediaPlayerWithInstructionsFragment);
+            Bundle bundle = new Bundle();
+            bundle.putString("videoUrl", videoUrl);
+            bundle.putString("thumbnail_url", thumbnailUrl);
+            bundle.putString("step_description", stepDescription);
+            mediaPlayerWithInstructionsFragment.setArguments(bundle);
+            fragmentTransaction.commit();
+
             return;
         }
         Intent intent = new Intent(this, DetailsActivity.class);
@@ -58,25 +62,10 @@ public class MasterListActivity extends AppCompatActivity implements MasterListF
         Toast.makeText(this, "Test " + videoUrl, Toast.LENGTH_LONG).show();
     }
 
-    private void addExoplayerFragment(String videoUrl, String stepDescription, String thumbnailUrl) {
-
-        fragmentTransaction = fragmentManager.beginTransaction();
-        MediaPlayerWithInstructionsFragment mediaPlayerWithInstructionsFragment = new MediaPlayerWithInstructionsFragment();
-        fragmentTransaction.replace(R.id.item_details_container, mediaPlayerWithInstructionsFragment);
-
-        Bundle bundle = new Bundle();
-        bundle.putString("videoUrl", videoUrl);
-        bundle.putString("thumbnail_url", thumbnailUrl);
-        bundle.putString("step_description", stepDescription);
-        mediaPlayerWithInstructionsFragment.setArguments(bundle);
-
-        fragmentTransaction.commit();
-    }
-
     @Override
     public void onIngredientCardInteraction() {
         if (twoPaneLayout) {
-            // show Ingredients
+            // show Ingredients fragment
             fragmentTransaction = fragmentManager.beginTransaction();
             ingredientListFragment = new IngredientsListFragment();
             fragmentTransaction.replace(R.id.item_details_container, ingredientListFragment);
